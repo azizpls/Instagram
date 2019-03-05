10bf3157f74df9990f2588920267bbfcc9b9a7f8
azizpls <andrewaziz@mac.com>
2019-03-04 19:33:29 -0500

    Added timeline and profile page using fragments. Added swipe to refresh.

---
+++ app/src/main/java/com/aaziz/instagram/fragments/PostsFragment.java
@@ -0,0 +1,104 @@
+package com.aaziz.instagram.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aaziz.instagram.Post;
+import com.aaziz.instagram.PostsAdapter;
+import com.aaziz.instagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostsFragment extends Fragment {
+
+    private SwipeRefreshLayout swipeContainer;
+
+    public static final String TAG = "PostsFragment";
+
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> mPosts;
+
+    //onCreate view to inflate the view
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // create the data source
+        mPosts = new ArrayList<>();
+        // create the adapter
+        adapter = new PostsAdapter(getContext(), mPosts);
+        // set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                Log.d("ParseIG", "Context is being refreshed");
+                queryPosts();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    protected void queryPosts() {
+        ParseQuery<Post> postQueuery = new ParseQuery<Post>(Post.class);
+        postQueuery.include(Post.KEY_USER);
+        postQueuery.setLimit(20);
+        postQueuery.addDescendingOrder(Post.KEY_CREATED_AT);
+        postQueuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "ERROR WITH QUERY");
+                    e.printStackTrace();
+                    return;
+                }
+
+                for (int i = 0; i < posts.size(); i++) {
+                    Post post = posts.get(i);
+                    Log.d(TAG, "Posts " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                // Show the data we just recieved
+                // Now we call setRefreshing(false) to signal refresh has finished
+                adapter.clear();
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
